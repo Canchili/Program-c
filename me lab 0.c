b1937e62fb90030dba477290e7d334448689aea0
Canchili <148684811+Canchili@users.noreply.github.com>
2024-02-10 13:24:33 +1000

    Update me lab 0.c

--- me lab 0.c
+++ me lab 0.c
@@ -1,8 +1,30 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 
 int main(int argc, char *argv[]) {
-	printf("Hello world")
+//	printf("Hello world");
+	double a = 5.1,b = 1,c = -2, d, x1, x2;
+	scanf("%lf",&a);
+	scanf("%lf",&b);
+	scanf("%lf",&c);
+	d = b*b - 4*a*c;
+	x1 = (-b + sqrt(d))/ (2*a);
+	x2 = (-b - sqrt(d))/ (2*a);
+	printf("d = %lf \n",d);
+	printf("x1 = %lf" "x2 = %lf",x1,x2);
+	if (d>0){
+	x1 = (-b + sqrt(d))/ (2*a);
+	x2 = (-b - sqrt(d))/ (2*a);
+	printf("x1 = %lf" "x2 = %lf",x1,x2);
+	}
+	if(d<0){
+		printf("korne net",0);
+	}
+	if (d == 0){
+		printf("1 korne",0);
+		x2 = (-b - sqrt(d))/ (2*a);
+		printf( "x2 = %lf",x2);
+	}
 	return 0;
 }
